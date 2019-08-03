73826f9194a1b04dbc8970be6daea6aa094bb3c0
LAPTOP-1IJF3F1M\97205 <972051267@qq.com>
2019-08-03 09:30:58 +0800

    主页 components 主页 个人中心组件 郑文谦

---
+++ components/nav.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <van-nav-bar
+      title="账户信息"
+      left-arrow
+      @click-left="onClickLeft"
+    />
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "nav",
+      methods:{
+        onClickLeft(){
+          this.$router.go(-1)
+        },
+      }
+    }
+</script>
+
+<style scoped>
+  .van-nav-bar__title{
+    color: white;
+    font-weight:700;
+    font-size: 0.18rem;
+  }
+  .van-nav-bar{
+    background: #3190e8;
+
+  }
+  .van-icon {
+    color: white;
+    font-size: 0.25rem;
+    margin-left: -100%;
+  }
+</style>
